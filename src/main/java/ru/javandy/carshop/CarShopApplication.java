d3299c212afe861683f009cb0604208e965585b0
JavAndy58 <digital.designs.russia@gmail.com>
2022-09-25 22:06:03 +0300

    creation dto model mapper

--- src/main/java/ru/javandy/carshop/CarShopApplication.java
+++ src/main/java/ru/javandy/carshop/CarShopApplication.java
@@ -1,10 +1,17 @@
 package ru.javandy.carshop;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CarShopApplication {
+
+    @Bean
+    public ModelMapper getMapper() {
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CarShopApplication.class, args);
