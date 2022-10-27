92f7070caf57e619163a307672d3015b4c5ff3ee
JavAndy58 <digital.designs.russia@gmail.com>
2022-10-27 18:13:48 +0300

    change scheme db

--- db/scripts/001_create.sql
+++ db/scripts/001_create.sql
@@ -2,14 +2,14 @@ CREATE TABLE IF NOT EXISTS cars
 (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    vinCode TEXT
+    vin_сode TEXT
 );
 
 CREATE TABLE IF NOT EXISTS customers
 (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    phoneNumber TEXT,
+    phone_number TEXT,
     cars_id INT NOT NULL REFERENCES cars(id)
 );
 
@@ -17,9 +17,8 @@ CREATE TABLE IF NOT EXISTS details
 (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    purchasePrice DOUBLE PRECISION ,
-    amount INT,
-    retailPrice DOUBLE PRECISION
+    purchase_price DOUBLE PRECISION ,
+    retail_price DOUBLE PRECISION
 );
 
 CREATE TABLE IF NOT EXISTS orders
@@ -27,7 +26,15 @@ CREATE TABLE IF NOT EXISTS orders
     id SERIAL PRIMARY KEY,
     created TIMESTAMP,
     customer_id INT NOT NULL REFERENCES customers(id),
-    detail_id INT NOT NULL REFERENCES details(id),
     prepayment DOUBLE PRECISION,
     delivered BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS sets
+(
+    id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL REFERENCES orders(id),
+    detail_id INT NOT NULL REFERENCES details(id),
+    amount INT
 )
+
---
+++ src/main/java/ru/javandy/carshop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package ru.javandy.carshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
