6ace144fb84188f631ec91b6b9c3f0ed40b2ee5e
JavAndy58 <digital.designs.russia@gmail.com>
2022-10-28 18:18:34 +0300

    change2 scheme db

--- db/scripts/001_create.sql
+++ db/scripts/001_create.sql
@@ -1,40 +1,39 @@
-CREATE TABLE IF NOT EXISTS cars
-(
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    vin_сode TEXT
-);
-
 CREATE TABLE IF NOT EXISTS customers
 (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    phone_number TEXT,
-    cars_id INT NOT NULL REFERENCES cars(id)
+    phone_number TEXT
 );
-
 CREATE TABLE IF NOT EXISTS details
 (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    purchase_price DOUBLE PRECISION ,
+    purchase_price DOUBLE PRECISION,
     retail_price DOUBLE PRECISION
 );
+CREATE TABLE IF NOT EXISTS cars
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    vin_сode TEXT,
+    customer_id INT NOT NULL REFERENCES customers(id),
 
+);
 CREATE TABLE IF NOT EXISTS orders
 (
     id SERIAL PRIMARY KEY,
     created TIMESTAMP,
-    customer_id INT NOT NULL REFERENCES customers(id),
     prepayment DOUBLE PRECISION,
-    delivered BOOLEAN
+    delivered BOOLEAN,
+    note TEXT,
+    customer_id INT NOT NULL REFERENCES customers(id),
+    car_id REFERENCES cars(id)
 );
-
 CREATE TABLE IF NOT EXISTS sets
 (
     id SERIAL PRIMARY KEY,
+    amount INT,
     order_id INT NOT NULL REFERENCES orders(id),
-    detail_id INT NOT NULL REFERENCES details(id),
-    amount INT
+    detail_id INT NOT NULL REFERENCES details(id)
 )
 
