54cda83e920af3a066f6d3380d18a613bd1a8b81
JavAndy58 <digital.designs.russia@gmail.com>
2022-11-11 18:12:15 +0300

    crud api

--- src/main/java/ru/javandy/carshop/controller/DetailController.java
+++ src/main/java/ru/javandy/carshop/controller/DetailController.java
@@ -1,6 +1,7 @@
 package ru.javandy.carshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,10 @@ import ru.javandy.carshop.service.DetailService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class DetailController {
-    private List<Detail> details;
 
     @Autowired
     private DetailService detailService;
