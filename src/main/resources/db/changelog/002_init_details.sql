ec662d367fb27f5ff1f7af85d40f70185321ebcf
JavAndy58 <digital.designs.russia@gmail.com>
2022-11-16 17:43:22 +0300

    fix error duplicate id

--- src/main/resources/db/changelog/002_init_details.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO details VALUES (1, 'Реле', 100 , 150);
-INSERT INTO details VALUES (2, 'Шрус', 1750 , 2100);
-INSERT INTO details VALUES (3, 'Магнитола', 2500 , 2980);
-INSERT INTO details VALUES (4, 'Клипса', 25 , 38);
