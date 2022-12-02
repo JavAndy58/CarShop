c1ff0380f4a60d0da9ecbf84059567f08385d4a1
JavAndy58 <digital.designs.russia@gmail.com>
2022-12-02 22:45:50 +0300

    create data init

--- src/main/java/ru/javandy/carshop/CarShopApplication.java
+++ src/main/java/ru/javandy/carshop/CarShopApplication.java
@@ -20,28 +20,27 @@ public class CarShopApplication {
         SpringApplication.run(CarShopApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner dataDetailLoader(DetailService detailService, CustomerService customerService,
-                                              CarService carService) {
-        Customer customerIvan = new Customer("Иван Петров", "+79273893138");
-        Customer customerEvg = new Customer("Ершое Евгений", "+72273893138");
-        Customer customerVlad = new Customer("Макаров Владимир", "+75553893138");
-
-
-        return args -> {
-            detailService.save(new Detail("Реле", 100, 150, false));
-            detailService.save(new Detail("Шрус", 1750, 2100, false));
-            detailService.save(new Detail("Рулевая рейка ", 10000, 12550, false));
-            detailService.save(new Detail("Клипса", 15, 50, false));
-            detailService.save(new Detail("Пыльник шруса", 985, 1270, false));
-
-            customerService.save(customerIvan);
-            customerService.save(customerEvg);
-            customerService.save(customerVlad);
-
-            carService.save(new Car("Focus 2", "XXEERTY525SA626", customerEvg));
-            carService.save(new Car("Logan 1", "TTTYYY525SA626", customerIvan));
-            carService.save(new Car("Audi 100", "YUTYRYREYRA626", customerIvan));
-        };
-    }
+//    @Bean
+//    public CommandLineRunner dataDetailLoader(DetailService detailService, CustomerService customerService,
+//                                              CarService carService) {
+//        Customer customerIvan = new Customer("Иван Петров", "+79273893138");
+//        Customer customerEvg = new Customer("Ершое Евгений", "+72273893138");
+//        Customer customerVlad = new Customer("Макаров Владимир", "+75553893138");
+//
+//        return args -> {
+//            detailService.save(new Detail("Реле", 100, 150, false));
+//            detailService.save(new Detail("Шрус", 1750, 2100, false));
+//            detailService.save(new Detail("Рулевая рейка ", 10000, 12550, false));
+//            detailService.save(new Detail("Клипса", 15, 50, false));
+//            detailService.save(new Detail("Пыльник шруса", 985, 1270, false));
+//
+//            customerService.save(customerIvan);
+//            customerService.save(customerEvg);
+//            customerService.save(customerVlad);
+//
+//            carService.save(new Car("Focus 2", "XXEERTY525SA626", customerEvg));
+//            carService.save(new Car("Logan 1", "TTTYYY525SA626", customerIvan));
+//            carService.save(new Car("Audi 100", "YUTYRYREYRA626", customerIvan));
+//        };
+//    }
 }
