90365a1edf86fa204f3f51b5f72347f908936c39
JavAndy58 <digital.designs.russia@gmail.com>
2022-12-05 21:43:12 +0300

    create Orders

--- src/main/java/ru/javandy/carshop/CarShopApplication.java
+++ src/main/java/ru/javandy/carshop/CarShopApplication.java
@@ -12,7 +12,6 @@ import ru.javandy.carshop.service.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -24,42 +23,42 @@ public class CarShopApplication {
         SpringApplication.run(CarShopApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner dataDetailLoader(DetailService detailService, CustomerService customerService,
-//                                              CarService carService, OrderService orderService) throws ParseException {
-//        Customer customerIvan = new Customer("Иван Петров", "+79273893138");
-//        Customer customerEvg = new Customer("Ершое Евгений", "+72273893138");
-//        Customer customerVlad = new Customer("Макаров Владимир", "+75553893138");
-//        List<Customer> customers = Arrays.asList(customerIvan, customerEvg, customerVlad);
-//
-//        Detail detailRel = new Detail("Реле", 2, 100, 150, "", false);
-//        Detail detailScr = new Detail("Шрус", 1, 1750, 2100, "", false);
-//        Detail detailRul = new Detail("Рулевая рейка ", 1, 10000, 12550, "", false);
-//        Detail detailClips = new Detail("Клипса", 10, 15, 50, "", false);
-//        Detail detailPl = new Detail("Пыльник шруса", 1, 985, 1270, "", false);
-//        List<Detail> details = Arrays.asList(detailRel, detailScr, detailRul, detailClips, detailPl);
-//
-//        Car carFocus = new Car("Focus 2", "XXEERTY525SA626", customerEvg);
-//        Car carLogan = new Car("Logan 1", "TTTYYY525SA626", customerIvan);
-//        Car carAudi = new Car("Audi 100", "YUTYRYREYRA626", customerIvan);
-//        List<Car> cars = Arrays.asList(carFocus, carLogan, carAudi);
-//
-//        List<Detail> detailOrderOne = Arrays.asList(detailRel, detailRul);
-//        List<Detail> detailOrderTwo = List.of(detailClips);
-//        List<Detail> detailOrderThree = Arrays.asList(detailPl,detailScr);
-//        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-//        Date dateOrderOne = format.parse("01.12.2022");
-//        Date dateOrderTwo = format.parse("03.12.2022");
-//        Date dateOrderThree = format.parse("05.12.2022");
-//
-//        return args -> {
-//            detailService.saveAll(details);
-//            customerService.saveAll(customers);
-//            carService.saveAll(cars);
-//
-//            orderService.save(new Order(dateOrderOne, 500, false, false, "", carFocus, detailOrderOne, customerEvg));
-//            orderService.save(new Order(dateOrderTwo, 0, false, true, "оплата по приезду", carLogan, detailOrderTwo, customerIvan));
-//            orderService.save(new Order(dateOrderThree, 100, false, false, " доставка месяц", carAudi, detailOrderThree, customerIvan));
-//        };
-//    }
+    @Bean
+    public CommandLineRunner dataDetailLoader(DetailService detailService, CustomerService customerService,
+                                              CarService carService, OrderService orderService) throws ParseException {
+        Customer customerIvan = new Customer("Иван Петров", "+79273893138");
+        Customer customerEvg = new Customer("Ершое Евгений", "+72273893138");
+        Customer customerVlad = new Customer("Макаров Владимир", "+75553893138");
+        List<Customer> customers = Arrays.asList(customerIvan, customerEvg, customerVlad);
+
+        Detail detailRel = new Detail("Реле", 2, 100, 150, "", false);
+        Detail detailScr = new Detail("Шрус", 1, 1750, 2100, "", false);
+        Detail detailRul = new Detail("Рулевая рейка ", 1, 10000, 12550, "", false);
+        Detail detailClips = new Detail("Клипса", 10, 15, 50, "", false);
+        Detail detailPl = new Detail("Пыльник шруса", 1, 985, 1270, "", false);
+        List<Detail> details = Arrays.asList(detailRel, detailScr, detailRul, detailClips, detailPl);
+
+        Car carFocus = new Car("Focus 2", "XXEERTY525SA626", customerEvg);
+        Car carLogan = new Car("Logan 1", "TTTYYY525SA626", customerIvan);
+        Car carAudi = new Car("Audi 100", "YUTYRYREYRA626", customerIvan);
+        List<Car> cars = Arrays.asList(carFocus, carLogan, carAudi);
+
+        List<Detail> detailOrderOne = Arrays.asList(detailRel, detailRul);
+        List<Detail> detailOrderTwo = List.of(detailClips);
+        List<Detail> detailOrderThree = Arrays.asList(detailPl, detailScr);
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date dateOrderOne = format.parse("01.12.2022");
+        Date dateOrderTwo = format.parse("03.12.2022");
+        Date dateOrderThree = format.parse("05.12.2022");
+
+        return args -> {
+            detailService.saveAll(details);
+            customerService.saveAll(customers);
+            carService.saveAll(cars);
+
+            orderService.save(new Order(dateOrderOne, 500, false, false, "", carFocus, detailOrderOne, customerEvg));
+            orderService.save(new Order(dateOrderTwo, 0, false, true, "оплата по приезду", carLogan, detailOrderTwo, customerIvan));
+            orderService.save(new Order(dateOrderThree, 100, false, false, " доставка месяц", carAudi, detailOrderThree, customerIvan));
+        };
+    }
 }
