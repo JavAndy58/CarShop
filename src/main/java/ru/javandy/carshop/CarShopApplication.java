ec081e6d5565780f8b9fc3e6e672471dd6d2e6e2
JavAndy58 <digital.designs.russia@gmail.com>
2022-12-22 22:19:21 +0300

    update data initialization

--- src/main/java/ru/javandy/carshop/CarShopApplication.java
+++ src/main/java/ru/javandy/carshop/CarShopApplication.java
@@ -8,14 +8,11 @@ import ru.javandy.carshop.model.Car;
 import ru.javandy.carshop.model.Customer;
 import ru.javandy.carshop.model.Detail;
 import ru.javandy.carshop.model.Order;
-import ru.javandy.carshop.repository.CarRepository;
 import ru.javandy.carshop.repository.CustomerRepository;
-import ru.javandy.carshop.repository.DetailRepository;
 import ru.javandy.carshop.repository.OrderRepository;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -41,8 +38,9 @@ public class CarShopApplication {
         Detail detailRul = new Detail("Рулевая рейка ", 1, 10000, 12550, "", false);
         Detail detailClips = new Detail("Клипса", 10, 15, 50, "", false);
         Detail detailPl = new Detail("Пыльник шруса", 1, 985, 1270, "", false);
+        Detail detailRadio = new Detail("Магнитола LG", 1, 7500, 10253, "", false);
 
-        Customer customerIvan = new Customer("Иван Петров", "+79273893138");
+        Customer customerIvan = new Customer("Иван Потапов", "+79273893138");
         Customer customerEvg = new Customer("Ершое Евгений", "+72273893138");
         Customer customerVlad = new Customer("Макаров Владимир", "+75553893138");
 
@@ -51,13 +49,12 @@ public class CarShopApplication {
         customerIvan.addCar(carLogan);
         customerEvg.addCar(carAudi);
         List<Customer> customers = Arrays.asList(customerIvan, customerEvg, customerVlad);
-
-//        Date dateOrderOne = new Date();
         SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy");
         Date dateOrderOne = formatDate.parse("01.12.2022");
-//        formatDate.format(dateOrderOne);
         Date dateOrderTwo = formatDate.parse("03.12.2022");
         Date dateOrderThree = formatDate.parse("05.12.2022");
+        Date dateOrderFour = formatDate.parse("07.12.2022");
+
 
         Order orderOne = new Order(dateOrderOne, 500, false, false, "", carAudi, customerEvg);
         orderOne.addDetail(detailRel);
@@ -67,7 +64,9 @@ public class CarShopApplication {
         orderTwo.addDetail(detailClips);
         Order orderThree = new Order(dateOrderThree, 100, false, false, " доставка месяц", carFocus, customerIvan);
         orderThree.addDetail(detailPl);
-        List<Order> orders = Arrays.asList(orderOne, orderTwo, orderThree);
+        Order orderFour = new Order(dateOrderFour, 5000, false, false, "", null, customerVlad);
+        orderFour.addDetail(detailRadio);
+        List<Order> orders = Arrays.asList(orderOne, orderTwo, orderThree, orderFour);
 
         return args -> {
 //            customerRepository.saveAll(customers);
