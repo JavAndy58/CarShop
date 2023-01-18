19271a894a924ea6349c10c3b31e1effe940af0b
JavAndy58 <digital.designs.russia@gmail.com>
2023-01-18 21:58:57 +0300

    create CarServiceImplTest

--- src/test/java/ru/javandy/carshop/service/CarServiceImplTest.java
+++ src/test/java/ru/javandy/carshop/service/CarServiceImplTest.java
@@ -5,26 +5,26 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import ru.javandy.carshop.dto.CarDto;
 import ru.javandy.carshop.mapper.CarMapper;
 import ru.javandy.carshop.model.Car;
 import ru.javandy.carshop.repository.CarRepository;
-import ru.javandy.carshop.service.CarServiceImpl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = CarServiceImpl.class)
+@SpringBootTest(classes = { CarServiceImpl.class, CarMapper.class })
 class CarServiceImplTest {
 
     @Autowired
-    private CarServiceImpl carService;
-
-//    @Autowired
-//    private CarMapper carMapper;
+    private CarService carService;
 
     @MockBean
     private CustomerService customerService;
@@ -42,39 +42,65 @@ class CarServiceImplTest {
     void getAllCarsWhenGetCarsThenStatus200() throws Exception {
         CarDto carDto1 = new CarDto(1, "Focus 2", "XXEERTY525SA626");
         CarDto carDto2 = new CarDto(2, "Logan 1", "TTTYYY525SA626");
-        List<CarDto> carDtoList = new ArrayList<>();
-        carDtoList.add(carDto1);
-        carDtoList.add(carDto2);
+        Car car1 = new Car(1, "Focus 2", "XXEERTY525SA626");
+        Car car2 = new Car(2, "Logan 1", "TTTYYY525SA626");
 
+        when(carMapper.toDto(car1)).thenReturn(carDto1);
+        when(carMapper.toDto(car2)).thenReturn(carDto2);
+        when(carRepository.findAll()).thenReturn(Arrays.asList(car1, car2));
 
-        Car car1 = carMapper.toEntity(carDto1);
-        Car car2 = carMapper.toEntity(carDto2);
-        List<Car> carList = new ArrayList<>();
-        carList.add(car1);
-        carList.add(car2);
-
-        Mockito.doReturn(carList).when(carRepository).findAll();
-        List<CarDto> actual = carService.getAllCars();
-
-        System.out.println("массив получился" + carList);
-        assertThat(actual).size().isEqualTo(2);
-//        assertThat(actual).isEqualTo(carDtoList);
-
-
-
-
+        carService.getAllCars();
+        verify(carRepository, times(1)).findAll();
+        verify(carMapper, times(1)).toDto(car1);
+        verify(carMapper, times(1)).toDto(car2);
     }
 
     @Test
     void saveCarWhenAddCarThenStatus200andCarReturned() throws Exception {
+        CarDto carDto1 = new CarDto(1, "Focus 2", "XXEERTY525SA626");
+        Car car1 = new Car(1, "Focus 2", "XXEERTY525SA626");
+
+        when(carMapper.toEntity(carDto1)).thenReturn(car1);
+        when(carRepository.save(car1)).thenReturn(car1);
+        when(carMapper.toDto(car1)).thenReturn(carDto1);
+
+        carService.saveCar(carDto1);
+        verify(carRepository, times(1)).save(car1);
+        verify(carMapper, times(1)).toEntity(carDto1);
+        verify(carMapper, times(1)).toDto(car1);
     }
 
     @Test
     void findByCarIdWhenGetExistingCarThenStatus200andCarReturned() throws Exception  {
+        int id = 1;
+        CarDto carDto1 = new CarDto(1, "Focus 2", "XXEERTY525SA626");
+        Car car1 = new Car(1, "Focus 2", "XXEERTY525SA626");
+        Optional<Car> optionalCar = Optional.of(car1);
+
+        when(carRepository.findById(id)).thenReturn(optionalCar);
+        when(carMapper.toDto(car1)).thenReturn(carDto1);
+
+        carService.findByCarId(id);
+        verify(carRepository, times(1)).findById(id);
+        verify(carMapper, times(1)).toDto(car1);
     }
 
     @Test
     void updateCarIdWhenUpdateThenStatus200andUpdateReturns() throws Exception {
+        int id = 1;
+        CarDto carDto1 = new CarDto(1, "Focus 2", "XXEERTY525SA626");
+        Car car = new Car(1, "Focus 2", "XXEERTY525SA626");
+//        Car car1 = new Car(1, "Focus 2", "XXEERTY525SA626");
+        car.setVinCode("848484");
+        Optional<Car> optionalCar = Optional.of(car);
+
+        when(carRepository.findById(id)).thenReturn(optionalCar);
+        when(carMapper.toDto(car)).thenReturn(carDto1);
+
+        carService.updateCarId(carDto1, id);
+        verify(carRepository, times(1)).findById(id);
+        verify(carRepository, times(1)).save(car);
+
     }
 
     @Test
